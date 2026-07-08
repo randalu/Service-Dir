5705560ed11fca773f5cbd556c6120f72e791b34
Antony Rodrigo <ant89lk@gmail.com>
2026-07-08 08:11:32 +0530

    Add embed.js script to footer

--- footer.php
+++ footer.php
@@ -31,5 +31,6 @@
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://zro.ovh/embed.js"></script>
 </body>
 </html>
